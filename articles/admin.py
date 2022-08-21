0152def29bcc44b4769e71f78636ea17dde23cc5
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-21 19:56:14 +0100

    fixed models.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Chapter
 
 
 @admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Chapter)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- articles/models.py
+++ articles/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +13,23 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название', blank=True)
+    article = models.ManyToManyField(Article,
+                                     through='ArticleChapter',
+                                     related_name='chapter')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleChapter(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(null=True)
