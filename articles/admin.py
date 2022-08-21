9c2a2e3313e6ea2bd6f1897ee41304abe1c47075
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-21 20:40:00 +0100

    in admin.py made InlineFormset

--- articles/admin.py
+++ articles/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Chapter, ArticleChapter
 
 
+class ArticleChapterInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        flag = 0
+        for form in self.forms:
+
+            if form.cleaned_data.get('DELETE'):
+                continue
+
+            if form.cleaned_data.get('is_main'):
+                flag += 1
+
+            if flag > 1:
+                raise ValidationError('Основным может быть только один раздел.')
+
+        if flag == 0:
+            raise ValidationError('Укажите основной раздел.')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
 class ArticleChapterInLine(admin.TabularInline):
     model = ArticleChapter
-    extra = 1
+    extra = 3
+    formset = ArticleChapterInlineFormset
 
 
 @admin.register(Article)
@@ -16,5 +38,3 @@ class ArticleAdmin(admin.ModelAdmin):
 @admin.register(Chapter)
 class ChapterAdmin(admin.ModelAdmin):
     list_display = ['name']
-
-
